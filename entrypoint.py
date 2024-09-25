cd2111b853ddfe9e2f0b2b6f846fef254c6cbd77
Nimi Williams <williamson.nimi@gmail.com>
2024-09-25 19:26:45 +0100

    Minor fix

--- entrypoint.py
+++ entrypoint.py
@@ -37,24 +37,14 @@ def record_engine_temperature():
 # practically identical to the above
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    payload = request.get_json(force=True)
-    logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
-
-    engine_temperature = payload.get("engine_temperature")
-    logger.info(f"engine temperature to record is: {engine_temperature}")
-
     database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
-    database.lpush(DATA_KEY, engine_temperature)
-    logger.info(f"stashed engine temperature in redis: {engine_temperature}")
-
-    while database.llen(DATA_KEY) > HISTORY_LENGTH:
-        database.rpop(DATA_KEY)
     engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
-    engine_temperature_values = [float(i) for i in engine_temperature_values]
+    logger.info(f"List of temperature values pulled from redis:{engine_temperature_values}")
     
-    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
+    engine_temperature_values2 = [float(i) for i in engine_temperature_values]
     
-    average_temperature = sum(engine_temperature_values)/len(engine_temperature_values)
+    average_temperature = sum(engine_temperature_values2)/len(engine_temperature_values2)
     logger.info(f"The average temperature of the engine is: {average_temperature}")
+    logger.info(f"The current temperature of the engine is: {engine_temperature_values[0]}")
     
-    return {"current_temperature": engine_temperature, "average_temperature": average_temperature}, 200
+    return {"current_temperature": engine_temperature_values[0], "average_temperature": average_temperature}, 200
