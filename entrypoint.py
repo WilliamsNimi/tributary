daf633bb177f82ef9b48a2d8e97fc6706b638654
Nimi Williams <williamson.nimi@gmail.com>
2024-09-25 19:17:54 +0100

    calculatiing the average temperature of the engine and returning the value in the collect route

--- entrypoint.py
+++ entrypoint.py
@@ -11,7 +11,6 @@ DATA_KEY = "engine_temperature"
 # create a Flask server, and allow us to interact with it using the app variable
 app = Flask(__name__)
 
-
 # define an endpoint which accepts POST requests, and is reachable from the /record endpoint
 @app.route('/record', methods=['POST'])
 def record_engine_temperature():
@@ -28,6 +27,7 @@ def record_engine_temperature():
     while database.llen(DATA_KEY) > HISTORY_LENGTH:
         database.rpop(DATA_KEY)
     engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    print(engine_temperature_values)
     logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
 
     logger.info(f"record request successful")
@@ -37,4 +37,24 @@ def record_engine_temperature():
 # practically identical to the above
 @app.route('/collect', methods=['POST'])
 def collect_engine_temperature():
-    return {"success": True}, 200
+    payload = request.get_json(force=True)
+    logger.info(f"(*) record request --- {json.dumps(payload)} (*)")
+
+    engine_temperature = payload.get("engine_temperature")
+    logger.info(f"engine temperature to record is: {engine_temperature}")
+
+    database = redis.Redis(host="redis", port=6379, db=0, decode_responses=True)
+    database.lpush(DATA_KEY, engine_temperature)
+    logger.info(f"stashed engine temperature in redis: {engine_temperature}")
+
+    while database.llen(DATA_KEY) > HISTORY_LENGTH:
+        database.rpop(DATA_KEY)
+    engine_temperature_values = database.lrange(DATA_KEY, 0, -1)
+    engine_temperature_values = [float(i) for i in engine_temperature_values]
+    
+    logger.info(f"engine temperature list now contains these values: {engine_temperature_values}")
+    
+    average_temperature = sum(engine_temperature_values)/len(engine_temperature_values)
+    logger.info(f"The average temperature of the engine is: {average_temperature}")
+    
+    return {"current_temperature": engine_temperature, "average_temperature": average_temperature}, 200
